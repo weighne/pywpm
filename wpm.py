7d92f44fc991ed1b11d804c75f5f57efbde5929e
Chris Vickery <vickeryc@newpaltz.edu>
2021-09-28 13:25:19 -0400

    Cleanup, still fixing math and error handling

--- wpm.py
+++ wpm.py
@@ -9,7 +9,7 @@ def gen_phrase():
         words = json.load(in_file)
     x = 0
     phrase = []
-    while x <= 10:
+    while x <= 20:
         wlist = words[random.choice(list(words.keys()))]
         index = random.randint(0, len(wlist)-1)
         phrase.append(wlist[index])
@@ -18,19 +18,21 @@ def gen_phrase():
 
 
 def calc_wpm(total_time, user_string, phrase):
-    wpm = total_time * 6
+    wpm = 60/total_time * len(phrase)  # extremely broken math
     mistakes = 0
     user_words = user_string.split(' ')
     for x in range(len(phrase)):
-        if user_words[x] == phrase[x]:
-            continue
-        else:
+        try:
+            if user_words[x] == phrase[x]:
+                continue
+            else:
+                mistakes+=1
+        except IndexError:
             mistakes+=1
+            continue
     return wpm, mistakes
 
 
-# with open('words.json','r') as in_file:
-#     words = json.load(in_file)
 phrase = gen_phrase()
 print((' ').join(phrase))
 
@@ -40,7 +42,7 @@ entry = str(input())
 end_time = datetime.datetime.now()
 total_time = (end_time - start_time).total_seconds()
 wpm, mistakes = calc_wpm(total_time, entry, phrase)
-print(f"WPM: {wpm}\nMistakes: {mistakes}")
+print(f"WPM: {wpm}\nMistakes: {mistakes}\nTime: {total_time}")
 
 # words[random list from dict][random value from list]
 # print(words[random.choice(list(words.keys()))][random.randint(0, len(words[random.choice(list(words.keys()))])-1)])
